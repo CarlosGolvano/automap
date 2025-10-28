d89ed23d4c83634e4ef0174917b60e7fe1cfda9c
CarlosGolvano <carlosgoolvin@gmail.com>
2025-10-28 10:21:27 +0100

    minor updated

--- datasets/blinkg/bin/remap.sh
+++ datasets/blinkg/bin/remap.sh
@@ -11,20 +11,15 @@ fi
 automap="$project/automap"
 dataset="$project/datasets/blinkg"
 # -------------------------
-scenario="scenario1/1A"
-exp="remap/run2"
+scenarios="scenario1/1A scenario1/1B scenario1/1C scenario1/1D scenario1/1E scenario1/1F scenario1/1G scenario1/1H"
+# scenarios="scenario1/1A scenario1/1B"
+
+# For a single run: use '_' or '-' (no slash to avoid creating subdirectories).
+# For multiple runs: use '/run1 /run2 /run3'. The slash is important to create different subdirectories.
+runs="_"
 # -------------------------
-data="$dataset/data/$scenario"
-exp_dir="$dataset/exps/$scenario/$exp"
 
-mkdir -p "$exp_dir"
-mkdir -p "$exp_dir/data"
-
-# Link data to exp directory. Just for easier access when reviewing results by hand.
-ln -sf $data/* $exp_dir/data
-ln -sf $data/../config.yaml $exp_dir/data/config.yaml
-
-# ==============================================================================
+# -------------------------
 # EXECUTABLES
 python="python"
 
@@ -32,72 +27,93 @@ python="python"
 remap="$python $automap/methods/remap/remap.py"
 
 # Evaluation
-eval="$python $automap/grapheval/compute_metrics.py"
+compute_metrics="$python $automap/grapheval/compute_metrics.py"
 
 # Postprocessing
 map2rml="$python $automap/converters/map2rml.py"
 rml_mapper="java -jar $project/resources/rmlmapper-8.0.0-r378-all.jar"
 
 #Visualization
-print_tabular="$python $automap/utils/visualizers.py"
+eval2tabular="$python $automap/utils/eval2tabular.py"
+eval2wb="$python $automap/utils/eval2wandb.py"
+# -------------------------
 
-# Data paths
-input_files="$data/student.csv"
+for scenario in $scenarios; do
+    for run in $runs; do
+        data="$dataset/data/$scenario"
+        exp="remap$run"
+        exp_dir="$dataset/exps/$scenario/$exp"
+        echo "----------------------------------------"
+        echo "Processing scenario: $scenario | Run: $run"
+        
+        mkdir -p "$exp_dir"
+        mkdir -p "$exp_dir/data"
 
-# Results paths
-mapping_yml_path="$exp_dir/mapping.yml"
-mapping_rml_path="$exp_dir/mapping.rml.ttl"
-pred_graph_path="$exp_dir/graph.nt"
-gold_graph_path="$data/gold_graph.nt"
-eval_results_path="$exp_dir/eval_results.json"
-logs_path="$exp_dir/logs"
-mkdir -p $logs_path
-# ==============================================================================
+        # Link data to exp directory. Just for easier access when reviewing results by hand.
+        ln -sf $data/* $exp_dir/data
+        ln -sf $data/../config.yaml $exp_dir/data/config.yaml
 
-# ==============================================================================
-# PREDICTIONS
-# ==============================================================================
+        # ==============================================================================
+        # Data paths
+        input_files="$data/*.csv"
+        echo $input_files
 
-$remap \
-    --csv $input_files \
-    --rdf $gold_graph_path \
-    --output $mapping_rml_path 2> $logs_path/remap.log
+        # Results paths
+        mapping_yml_path="$exp_dir/mapping.yml"
+        mapping_rml_path="$exp_dir/mapping.rml.ttl"
+        pred_graph_path="$exp_dir/graph.nt"
+        gold_graph_path="$data/gold_graph.nt"
+        eval_results_path="$exp_dir/eval_results.json"
+        tabular_results_path="$exp_dir/eval_results.tsv"
 
-# ==============================================================================
-# ==============================================================================
-# MAPPING TO RML AND GRAPH GENERATION
-# ==============================================================================
+        logs_path="$exp_dir/logs"
+        mkdir -p $logs_path
+        # ==============================================================================
 
-# ReMap already outputs RML
-# cat $mapping_yml_path | $map2rml > $mapping_rml_path 2> $logs_path/map2rml.log
+        # ==============================================================================
+        # PREDICTIONS
+        # ==============================================================================
 
-touch $pred_graph_path
-$rml_mapper \
-    -m $mapping_rml_path \
-    -o $pred_graph_path 2> $logs_path/rmlmapper.log
+        $remap \
+            --csv $input_files \
+            --rdf $gold_graph_path \
+            --output $mapping_rml_path 2> $logs_path/remap.log
 
-# ==============================================================================
+        # ==============================================================================
+        # ==============================================================================
+        # MAPPING TO RML AND GRAPH GENERATION
+        # ==============================================================================
 
-# ==============================================================================
-# EVALUATION
-# ==============================================================================
+        # ReMap already outputs RML
+        # cat $mapping_yml_path | $map2rml > $mapping_rml_path 2> $logs_path/map2rml.log
 
-# Temp. It is needed to sort the files. This should be fixed in the code.
-cat $gold_graph_path | sort > $exp_dir/gold_graph.nt.sorted
-mv $exp_dir/gold_graph.nt.sorted $exp_dir/gold_graph.nt
+        touch $pred_graph_path
+        $rml_mapper \
+            -m $mapping_rml_path \
+            -o $pred_graph_path 2> $logs_path/rmlmapper.log
 
-cat $pred_graph_path | sort > $pred_graph_path.sorted
-mv $pred_graph_path.sorted $pred_graph_path
+        # ==============================================================================
 
-cat $pred_graph_path | $eval \
-    --config $data/../config.yaml \
-    --pred_mapping $mapping_rml_path \
-    --gold_graph $gold_graph_path > $eval_results_path \
-    --only_common
-# ==============================================================================
+        # ==============================================================================
+        # EVALUATION
+        # ==============================================================================
 
-# ==============================================================================
-# VISUALIZATION
-# ==============================================================================
+        # Temp. It is needed to sort the files. This should be fixed in the code.
+        cat $gold_graph_path | sort > $exp_dir/gold_graph.nt.sorted
+        mv $exp_dir/gold_graph.nt.sorted $exp_dir/gold_graph.nt
 
+        cat $pred_graph_path | sort > $pred_graph_path.sorted
+        mv $pred_graph_path.sorted $pred_graph_path
 
+        cat $pred_graph_path | $compute_metrics \
+            --config $data/../config.yaml \
+            --pred_mapping $mapping_rml_path \
+            --gold_graph $gold_graph_path > $eval_results_path
+        # ==============================================================================
+
+        # ==============================================================================
+        # VISUALIZATION
+        # ==============================================================================
+        cat $eval_results_path | $eval2tabular > $tabular_results_path
+    done
+done
--- resources/exp_bin_template.sh
+++ resources/exp_bin_template.sh
@@ -9,85 +9,111 @@ fi
 
 # Directories paths
 automap="$project/automap"
-dataset="$project/datasets/<dataset>"
+dataset="$project/datasets/blinkg"
 # -------------------------
-scenario="<scenario>/<...>"
-exp="<exp>/<...>"
+scenarios="scenario1/1A scenario1/1B scenario1/1C scenario1/1D scenario1/1E scenario1/1F scenario1/1G scenario1/1H"
+# scenarios="scenario1/1A scenario1/1B"
+
+# For a single run: use '_' or '-' (no slash to avoid creating subdirectories).
+# For multiple runs: use '/run1 /run2 /run3'. The slash is important to create different subdirectories.
+runs="_"
 # -------------------------
-data="$dataset/data/$scenario"
-exp_dir="$dataset/exps/$scenario/$exp"
 
-mkdir -p "$exp_dir"
-mkdir -p "$exp_dir/data"
-
-# Link data to exp directory. Just for easier access when reviewing results by hand.
-ln -sf $data/* $exp_dir/data
-ln -sf $data/../config.yaml $exp_dir/data/config.yaml
-
-# ==============================================================================
+# -------------------------
 # EXECUTABLES
 python="python"
 
 # Method
-remap="$python $automap/methods/<method>/<method>.py"
+remap="$python $automap/methods/remap/remap.py"
 
 # Evaluation
-eval="$python $automap/grapheval/compute_metrics.py"
+compute_metrics="$python $automap/grapheval/compute_metrics.py"
 
 # Postprocessing
 map2rml="$python $automap/converters/map2rml.py"
 rml_mapper="java -jar $project/resources/rmlmapper-8.0.0-r378-all.jar"
 
-# Data paths
-input_files="$data/student.csv"
+#Visualization
+eval2tabular="$python $automap/utils/eval2tabular.py"
+eval2wb="$python $automap/utils/eval2wandb.py"
+# -------------------------
 
-# Results paths
-mapping_yml_path="$exp_dir/mapping.yml"
-mapping_rml_path="$exp_dir/mapping.rml.ttl"
-pred_graph_path="$exp_dir/graph.nt"
-gold_graph_path="$data/gold_graph.nt"
-eval_results_path="$exp_dir/eval_results.json"
-logs_path="$exp_dir/logs"
-mkdir -p $logs_path
-# ==============================================================================
+for scenario in $scenarios; do
+    for run in $runs; do
+        data="$dataset/data/$scenario"
+        exp="remap$run"
+        exp_dir="$dataset/exps/$scenario/$exp"
+        echo "----------------------------------------"
+        echo "Processing scenario: $scenario | Run: $run"
+        
+        mkdir -p "$exp_dir"
+        mkdir -p "$exp_dir/data"
 
-# ==============================================================================
-# METHOD PREDICTIONS
-# ==============================================================================
+        # Link data to exp directory. Just for easier access when reviewing results by hand.
+        ln -sf $data/* $exp_dir/data
+        ln -sf $data/../config.yaml $exp_dir/data/config.yaml
 
-$remap \
-    --csv $input_files \
-    --rdf $gold_graph_path \
-    --output $mapping_rml_path 2> $logs_path/remap.log
+        # ==============================================================================
+        # Data paths
+        input_files="$data/*.csv"
+        echo $input_files
 
-# ==============================================================================
-# ==============================================================================
-# MAPPING TO RML AND GRAPH GENERATION
-# ==============================================================================
+        # Results paths
+        mapping_yml_path="$exp_dir/mapping.yml"
+        mapping_rml_path="$exp_dir/mapping.rml.ttl"
+        pred_graph_path="$exp_dir/graph.nt"
+        gold_graph_path="$data/gold_graph.nt"
+        eval_results_path="$exp_dir/eval_results.json"
+        tabular_results_path="$exp_dir/eval_results.tsv"
 
-# YARRRML to RML
-cat $mapping_yml_path | $map2rml > $mapping_rml_path 2> $logs_path/map2rml.log
+        logs_path="$exp_dir/logs"
+        mkdir -p $logs_path
+        # ==============================================================================
 
-# RML to GRAPH
-touch $pred_graph_path
-$rml_mapper \
-    -m $mapping_rml_path \
-    -o $pred_graph_path 2> $logs_path/rmlmapper.log
+        # ==============================================================================
+        # PREDICTIONS
+        # ==============================================================================
 
-# ==============================================================================
-# ==============================================================================
-# EVALUATION
-# ==============================================================================
+        $remap \
+            --csv $input_files \
+            --rdf $gold_graph_path \
+            --output $mapping_rml_path 2> $logs_path/remap.log
 
-# Temp. It is needed to sort the files. This should be fixed in the code.
-cat $gold_graph_path | sort > $exp_dir/gold_graph.nt.sorted
-mv $exp_dir/gold_graph.nt.sorted $exp_dir/gold_graph.nt
+        # ==============================================================================
+        # ==============================================================================
+        # MAPPING TO RML AND GRAPH GENERATION
+        # ==============================================================================
 
-cat $pred_graph_path | sort > $pred_graph_path.sorted
-mv $pred_graph_path.sorted $pred_graph_path
+        # ReMap already outputs RML
+        # cat $mapping_yml_path | $map2rml > $mapping_rml_path 2> $logs_path/map2rml.log
 
-cat $pred_graph_path | $eval \
-    --config $data/../config.yaml \
-    --pred_mapping $mapping_rml_path \
-    --gold_graph $gold_graph_path > $eval_results_path 
-# ==============================================================================
+        touch $pred_graph_path
+        $rml_mapper \
+            -m $mapping_rml_path \
+            -o $pred_graph_path 2> $logs_path/rmlmapper.log
+
+        # ==============================================================================
+
+        # ==============================================================================
+        # EVALUATION
+        # ==============================================================================
+
+        # Temp. It is needed to sort the files. This should be fixed in the code.
+        cat $gold_graph_path | sort > $exp_dir/gold_graph.nt.sorted
+        mv $exp_dir/gold_graph.nt.sorted $exp_dir/gold_graph.nt
+
+        cat $pred_graph_path | sort > $pred_graph_path.sorted
+        mv $pred_graph_path.sorted $pred_graph_path
+
+        cat $pred_graph_path | $compute_metrics \
+            --config $data/../config.yaml \
+            --pred_mapping $mapping_rml_path \
+            --gold_graph $gold_graph_path > $eval_results_path
+        # ==============================================================================
+
+        # ==============================================================================
+        # VISUALIZATION
+        # ==============================================================================
+        cat $eval_results_path | $eval2tabular > $tabular_results_path
+    done
+done
